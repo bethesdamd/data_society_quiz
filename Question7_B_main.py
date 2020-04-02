a459dd091823fdb7fd76fa076d164b384dd8378b
David Swearingen <swearingendw@mac.com>
2020-04-02 15:27:39 -0400

    working

---
+++ Question7_B_main.py
@@ -0,0 +1,21 @@
+# Question 7 B
+# This is the main program which calls a simulated service,
+# called process() in module Question7_B_module
+
+from money import Money
+import Question7_B_module as q
+import json
+
+if __name__ == "__main__":
+
+    # Zero out the queue value
+    with open('total_queue.txt', 'w') as f:
+        f.write("0.0")
+
+    data = json.load(open('input.json', 'r'))
+
+    final_arr = []
+    with open('output_a.json', 'w') as f:
+        for rec in data:
+            final_arr.append(q.process(rec))
+        json.dump(final_arr, f, indent=4)
--- Question7_B_module.py
+++ Question7_B_module.py
@@ -8,16 +8,12 @@ def write_total_to_simulated_queue(money):
         f.write(str(money.amount))
 
 # Read total from queue
-# returns as formatted string e.g. '$44.15'
+# returns as type Money
 def read_total_from_simulated_queue():
     with open('total_queue.txt', 'r') as f:
         v = f.readline().strip()
-        return Money(v, currency='USD').format('en_US')
+        return Money(v, currency='USD')
 
-
-x = Money('1234144.12', currency='USD')
-write_total_to_simulated_queue(x)
-print(read_total_from_simulated_queue())
 
 def process(rec):
     dollar_sign_regex = re.compile(r'^\$(.+)$')
@@ -26,11 +22,12 @@ def process(rec):
     bal_fixed = bal_fixed.replace(',', '')
     this_balance = Money(bal_fixed, currency='USD')
 
-    total += this_balance
-    total_as_string = str(total.format('en_US'))
-    rec['balance'] = total_as_string
-
-    out_list.append(rec)
+    total_money = this_balance + read_total_from_simulated_queue()
+    print(f'total_money: {total_money}')
+    write_total_to_simulated_queue(total_money)
+    # total_as_string = str(total.format('en_US'))
+    rec['balance'] = total_money.format('en_US')
+    return rec
 
 #with open('output_a.json', 'w') as f:
 #    json.dump(out_list, f, indent=4)
