c4590b24d9e3c2bb6625df8e2efa2504218e89fd
David Swearingen <swearingendw@mac.com>
2020-04-02 14:30:15 -0400

    in progress

---
+++ Question7-B-module.py
@@ -0,0 +1,37 @@
+import json
+import re
+from money import Money
+
+# Writes total to simulated message queue (as string)
+def write_total_to_simulated_queue(money_amount):
+    with open('total_queue.txt', 'w') as f:
+        f.write(money_amount.format('en_US'))
+
+# Read total from queue
+# returns as formatted string e.g. '$44.15'
+def read_total_from_simulated_queue():
+    with open('total_queue.txt', 'r') as f:
+        v = f.readline().strip()
+        return v.format('en_US')
+
+
+x = Money('44.00', currency='USD')
+write_total_to_simulated_queue(x)
+print(read_total_from_simulated_queue())
+
+
+def process(rec):
+    dollar_sign_regex = re.compile(r'^\$(.+)$')
+    bal_string = rec['balance']
+    bal_fixed = dollar_sign_regex.search(bal_string).group(1)
+    bal_fixed = bal_fixed.replace(',', '')
+    this_balance = Money(bal_fixed, currency='USD')
+
+    total += this_balance
+    total_as_string = str(total.format('en_US'))
+    rec['balance'] = total_as_string
+
+    out_list.append(rec)
+
+#with open('output_a.json', 'w') as f:
+#    json.dump(out_list, f, indent=4)
